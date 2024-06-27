252ea3a5db4606f16661a0362f2b42d2c94ea219
Dan <dqdo873@gmail.com>
2024-06-27 01:16:04 -0400

    Created mods section sidebar (no content inside yet). The sidebar slides open and closes when the mods toggle button is clicked.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function modsToggleButtonAnimation() {
   const svg = document.getElementById("anyPolarity");
   const paths = svg.getElementsByTagName("path");
+  const sidebar = document.getElementById('modsSidebar');
+
+   sidebar.classList.toggle('open');
+
+  // Toggle SVG Rotation for any_polarity.svg
   svg.classList.toggle("rotatedAnyPolarity");
+
+  // Toggle SVG Color for any_polarity.svg
   for (let i = 0; i < paths.length; i++) {
     if (paths[i].getAttribute("fill") === "#FF0000") {
       paths[i].setAttribute("fill", "#00FF00");
